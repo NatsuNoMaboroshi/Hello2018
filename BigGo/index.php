0919fba47d96653da514396af0b5395629d8bf3f
uWayLu <u.master.o.twn@gmail.com>
2022-09-07 00:59:47 +0800

    feat: add biggo zhcn converter/fmt

--- BigGo/index.php
+++ BigGo/index.php
@@ -16,6 +16,7 @@ class ShopeeCataLog
     protected $perPage = 60;
     protected $pages = [];
     protected $curl_opts = [];
+    protected $converters = [];
 
     public function __construct($curl_opts = [])
     {
@@ -23,8 +24,16 @@ class ShopeeCataLog
         $apiData = $this->fetch();
         $this->metaData = [
             'total_count' => $apiData['total_count'] ?? 0,
+            'page_start' => 0,
+            'page_end' => (int) max(ceil($apiData['total_count'] / $this->perPage) - 1, 0),
         ];
         $this->pages[0] = $apiData['items'] ?? [];
+    }
+
+    public function setConverter($lang, $converter)
+    {
+        $this->converters[$lang] = $converter;
+        return $this;
     }
 
 
@@ -41,6 +50,8 @@ class ShopeeCataLog
             'scenario' => 'PAGE_CATEGORY',
             'version' => '2',
         ]);
+
+        var_dump($qStr);
 
         $ch = curl_init();
         curl_setopt_array($ch, $curl_opts + $this->curl_opts + [
@@ -63,10 +74,83 @@ class ShopeeCataLog
      */
     public function getPage($page): array
     {
-        if (!isset($this->pages[$page]))
+        if (!isset($this->pages[$page])) {
+            echo "fetching page $page...", "\n";
             $this->pages[$page] = $this->fetch(['newest' => $this->perPage * $page])['items'];
-        return $this->pages[$page];
+        }
+        return $this->pages[$page] ?? [];
+    }
+
+    public function getAllPages(): array
+    {
+        for ($i = $this->metaData['page_start']; $i <= $this->metaData['page_end']; $i++) {
+            $this->getPage($i);
+        }
+        return $this->pages;
+    }
+
+    /**
+     * Reserve
+     */
+    public function getPageWithTrans($page, $lang = 'cn')
+    {
+        // $page = $this->getPage($page);
+        // $converter = $this->converters[$lang] ?? null;
+        // return !!$converter ? $converter->convert($page) : $page;
+    }
+
+    /**
+     * formatter
+     * 
+     * @return $products by page
+     */
+    public function toBigGo($pageNum = null)
+    {
+        $bigGoFmt = function ($product) {
+            return [
+                'itemid' => $product['item_basic']['itemid'],
+                'name' => $product['item_basic']['name'],
+                'name_cn' => $this->converters['cn']->convert($product['item_basic']['name']),
+                'price' => (int) ceil($product['item_basic']['price'] / 100000),
+                'price_min' => (int) ceil($product['item_basic']['price_min'] / 100000),
+                'price_max' => (int) ceil($product['item_basic']['price_max'] / 100000),
+            ];
+        };
+
+        return array_map(function ($page) use ($bigGoFmt) {
+            return array_map($bigGoFmt, $page);
+        }, is_null($pageNum) ? $this->getAllPages() : [$pageNum => $this->getPage($pageNum)]);
     }
 }
 
-$catalog = new ShopeeCataLog();
+class ConverterZHCN
+{
+    public function convert(string $str)
+    {
+        $str = str_replace(array_keys(ZhConversion::$zh2CN), ZhConversion::$zh2CN, $str);
+        $str = str_replace(array_keys(ZhConversion::$zh2Hans), ZhConversion::$zh2Hans, $str);
+        return $str;
+    }
+
+    public function convertMap(array $arr)
+    {
+        return array_map([$this, 'convert'], $arr);
+    }
+}
+
+$catalog = (new ShopeeCataLog)->setConverter('cn', new ConverterZHCN);
+
+// products with detail of specific page
+// $products = $catalog->getPage(0);
+
+// all products with detail by page
+// $products = $catalog->getAllPages();
+
+// products of specific page
+// $products = $catalog->toBigGo(0);
+
+// all products by page
+$products = $catalog->toBigGo();
+
+// play with console like `php index.php | less`
+print_r($products);
